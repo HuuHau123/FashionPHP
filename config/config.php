596304c8de03c7968f90ace31125fef368eba7a1
HuuHau123 <146198120+HuuHau123@users.noreply.github.com>
2024-05-21 21:19:07 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-define("DB_HOST", 'dtdmvmu.mysql.database.azure.com');
+define("DB_HOST", 'mysqlvmu.mysql.database.azure.com');
 define("DB_USER", 'hauhihi');
 define("DB_PASS", 'Saobang1@');
 define("DB_NAME", 'instrumentstore');
