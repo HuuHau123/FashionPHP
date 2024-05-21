ba4369b3ce4779aaf738e833b80aab0adb6763bd
HuuHau123 <146198120+HuuHau123@users.noreply.github.com>
2024-05-21 22:54:04 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,5 +3,5 @@ define("DB_HOST", 'mysqlvmu.mysql.database.azure.com');
 define("DB_USER", 'hauhihi');
 define("DB_PASS", 'Saobang1@');
 define("DB_NAME", 'instrumentstore');
-define("BASE_URL", "https://shopvmu.azurewebsites.net/");
+
 ?>
